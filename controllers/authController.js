cb2e0054fdac29c25da524dc6ccfb4a77522f56d
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-02 17:49:11 +0530

    verify usre

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,29 @@
 const { User } = require("../models/Index");
-const generateToken = require("../utils/generateToken")
+const generateToken = require("../utils/generateToken");
+const { Op } = require("sequelize");
+const { sendEmail } = require("../utils/sendEmail");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
-  const { name, email,phone, password } = req.body;
+  const { name, email, phone, password } = req.body;
   try {
-    const existingUser = await User.findOne({ where: { email } });
+    const existingUser = await User.findOne({
+      where: { [Op.or]: [{ email }, { phone }] },
+    });
     if (existingUser) {
-      return res.status(400).json({ status:400, message: "Email already registered." });
-    }
+      if (existingUser.email === email) {
+        return res.status(409).json({
+          status: 409,
+          message: "Email already registered.",
+        });
+      }
 
-     const existingPhone = await User.findOne({ where: { phone } });
-    if (existingPhone) {
-      return res.status(400).json({ status:400, message: "Phone Number already registered." });
+      if (existingUser.phone === phone) {
+        return res
+          .status(409)
+          .json({ status: 409, message: "Phone Number already registered." });
+      }
     }
-
 
     // create new user
     const newUser = await User.create({
@@ -23,37 +33,72 @@ exports.register = async (req, res) => {
       password,
     });
 
+    res
+      .status(200)
+      .json({
+        status: 200,
+        message: "Registration successful, please verify your email!",
+      });
 
-    return res.status(200).json({
-      message: "User registered successfully",
-      user: {
-        user_id: newUser.user_id,
-        name: newUser.name,
-        email: newUser.email,
-        phone: newUser.phone,
-        role: newUser.role
-      },
-    });
+    // Create verification token
+    const token = jwt.sign(
+      { user_id: newUser.user_id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
 
+    const verifyLink = `http://localhost:4000/api/v1/auth/verify-user/${token}`;
+    await sendEmail(
+      email,
+      "Use This Link to Activate Your Account",
+      `Please verify your account.`,
+      `<p>Click to verify: <a href="${verifyLink}">${verifyLink}</a></p>`
+    );
   } catch (error) {
-    return res.status(500).json({ status:500, message: "Server Error"});
+    return res.status(500).json({ status: 500, message: "Server Error" });
   }
 };
 
-
-exports.login = async (req, res) =>{
-  const { email, password } = req.body
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({where:{email}})
+    const user = await User.findOne({ where: { email } });
 
-    if(!user){
-         if (!user) return res.status(400).json({ message: "User not found" });
+    if (!user) {
+      if (!user) return res.status(400).json({ message: "User not found" });
     }
-     const isMatch = password === user.password
-     if(!isMatch){
-      return res.status(400).json({status:400, message:"Invalid credentials"})
-     }
+    const isMatch = password === user.password;
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Invalid credentials" });
+    }
+
+    if (!user.isVerify) {
+      // Create verification token
+      const token = jwt.sign(
+        { user_id: user.user_id },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "1d",
+        }
+      );
+      const verifyLink = `http://localhost:4000/api/v1/auth/verify-user/${token}`;
+      await sendEmail(
+        email,
+        "Use This Link to Activate Your Account",
+        `Please verify your account.`,
+        `<p>Click to verify: <a href="${verifyLink}">${verifyLink}</a></p>`
+      );
+      return res.status(201).json({
+        status: 201,
+        message: "Please Check your email to verify your account.",
+      });
+    }
+
     return res.json({
       message: "Login successful",
       token: generateToken(user.user_id),
@@ -61,12 +106,36 @@ exports.login = async (req, res) =>{
         user_id: user.user_id,
         name: user.name,
         email: user.email,
-        profileImage:user.profileImage,
-        role: user.role
-      }
+        profileImage: user.profileImage,
+        role: user.role,
+      },
     });
   } catch (error) {
-      return res.status(500).json({status:500, message: "Server error" });
+    return res.status(500).json({ status: 500, message: "Server error" });
   }
-}
+};
 
+exports.verifyUser = async (req, res) => {
+  try {
+    const { token } = req.params;
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Update use
+    const user = await User.findByPk(decoded.user_id);
+    if (!user)
+      return res.status(404).json({ status: 404, message: "User not found" });
+
+    user.isVerify = true;
+    await user.save();
+    return res
+      .status(200)
+      .json({ status: 200, message: "Account verified successfully!" });
+  } catch (error) {
+    res.status(400).json({ status: 400, message: "Invalid or expired token" });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  res.json({ message: "Forget Password." });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/authController");
+const { register, login, verifyUser } = require("../controllers/authController");
 
 router.post("/register",register);
 router.post("/login",login);
+router.get("/verify-user/:token",verifyUser);
 
 module.exports = router;
