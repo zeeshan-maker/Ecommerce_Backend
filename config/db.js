3f4d004b8c979af521893a2ba37050a6b6801124
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-15 19:51:53 +0530

    change db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,   // database name
+//   process.env.DB_USER,   // username
+//   process.env.DB_PASS,   // password
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false, // disable SQL logs in console
+//   }
+// );
 
 const sequelize = new Sequelize(process.env.DATABASE_URL,
   {
