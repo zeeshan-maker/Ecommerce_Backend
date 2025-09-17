c582d7a22466bc59024560a0f41028012f0d0740
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-17 12:23:52 +0530

    change db

--- config/db.js
+++ config/db.js
@@ -1,29 +1,29 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,   // database name
-  process.env.DB_USER,   // username
-  process.env.DB_PASS,   // password
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false, // disable SQL logs in console
-  }
-);
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL,
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,   // database name
+//   process.env.DB_USER,   // username
+//   process.env.DB_PASS,   // password
 //   {
-//     dialect: process.env.DB_dialect, 
-//      dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false, // Important for Render SSL
-//       },
-//     }
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false, // disable SQL logs in console
 //   }
-
 // );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL,
+  {
+    dialect: process.env.DB_dialect, 
+     dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Important for Render SSL
+      },
+    }
+  }
+
+);
 
 
 module.exports = sequelize
