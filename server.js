a45142af42f3f0f6def13feb6fcad4c72adf1d29
zeeshan-techtorch <zeeshan.khan@techtorch.solutions>
2025-09-18 08:46:24 +0530

    chnage cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL,credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
